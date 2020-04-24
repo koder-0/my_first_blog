59fa4ffd982a1dfa62437a0e3fd941554d9c2e24
kaushalkumarroy <kaushal.dakshana16@gmail.com>
2020-04-24 13:14:00 +0530

    removed redundant files

--- first_site/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('posts/<int:pk>/', views.post_detail, name='post_detail'),
-    path('posts/new/', views.post_new, name='post_new'),
-    path('posts/<int:pk>/edit', views.post_edit, name='post_edit'),
-    ]
